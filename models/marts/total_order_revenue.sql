315dc979e24d448bdce390740b345ad5580eaff1
Tim Martin <tim.martin@dbtlabscoalesce.com>
2023-10-17 19:57:26 +0000

    m

--- models/marts/total_order_revenue.sql
+++ models/marts/total_order_revenue.sql
@@ -11,3 +11,19 @@ agg as (
 )
 
 select * from agg
+
+
+
+ with orders as (
+     select * from {{ ref('core_platform', 'fct_orders') }}
+ ), 
+
+ agg as (
+     select 
+         ordered_at,
+         sum(order_total) as order_revenue 
+     from orders 
+     group by 1
+ )
+
+ select * from agg
